e9c8934364d44eb3b3c409f9a1fecfb1aa5de316
Tran Van Charles <CTranVan.prestataire@voyages-sncf.com>
2016-03-07 16:03:00 +0100

    initial commit

---
+++ src/main/resources/static/js/hello.js
@@ -0,0 +1,75 @@
+angular.module('hello', [ 'ngRoute' ]).config(function($routeProvider, $httpProvider) {
+
+	$routeProvider.when('/', {
+		templateUrl : 'login.html',
+		controller : 'navigation',
+		controllerAs: 'controller'
+	}).when('/home', {
+		templateUrl : 'home.html',
+		controller : 'home',
+		controllerAs: 'controller'
+	}).otherwise('/');
+
+	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+}).controller('navigation',
+
+		function($rootScope, $http, $location, $route) {
+			
+			var self = this;
+
+			self.tab = function(route) {
+				return $route.current && route === $route.current.controller;
+			};
+
+			var authenticate = function(credentials, callback) {
+
+				var headers = credentials ? {
+					authorization : "Basic "
+							+ btoa(credentials.username + ":"
+									+ credentials.password)
+				} : {};
+
+				$http.get('user', {
+					headers : headers
+				}).success(function(data) {
+					if (data.name) {
+						$rootScope.authenticated = true;
+					} else {
+						$rootScope.authenticated = false;
+					}
+					callback && callback($rootScope.authenticated);
+				}).error(function() {
+					$rootScope.authenticated = false;
+					callback && callback(false);
+				});
+
+			}
+
+			authenticate();
+
+			self.credentials = {};
+			self.login = function() {
+				authenticate(self.credentials, function(authenticated) {
+					if (authenticated) {
+						console.log("Login succeeded")
+						$location.path("/home");
+						self.error = false;
+						$rootScope.authenticated = true;
+					} else {
+						console.log("Login failed")
+						$location.path("/login");
+						self.error = true;
+						$rootScope.authenticated = false;
+					}
+				})
+			};
+
+			self.logout = function() {
+				$http.post('logout', {}).finally(function() {
+					$rootScope.authenticated = false;
+					$location.path("/");
+				});
+			}
+
+		});
